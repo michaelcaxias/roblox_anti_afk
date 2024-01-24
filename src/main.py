9375045eaace075c62849046fdb6fbeca4b35e09
Michael Caxias <michaelcaxias.dev@gmail.com>
2024-01-24 19:39:43 -0300

    feat:  print useful info

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import pyautogui
 import pygetwindow as gw
+import time
 
 import utils.keyboard_interaction as ki
 from utils.keys import hex_key
-import time
 
 FIFTEEN_MINUTES = 900
 
 def active_roblox_window():
     try:
-        roblox_window = gw.getWindowsWithTitle('Roblox')[0]
+        roblox_window = gw.getWindowsWithTitle('Roblox')[1]
 
         left = roblox_window.left
         top = roblox_window.top
@@ -21,16 +21,19 @@ def active_roblox_window():
         print("Roblox window not found")
 
 def move_around_yourself():
-    ki.pressKey(hex_key.get('w'), 0.3)
-    ki.pressKey(hex_key.get('a'), 0.3)
-    ki.pressKey(hex_key.get('s'), 0.3)
-    ki.pressKey(hex_key.get('d'), 0.3)
+    ki.pressKey(hex_key.get('W'), 0.3)
+    ki.pressKey(hex_key.get('A'), 0.3)
+    ki.pressKey(hex_key.get('S'), 0.3)
+    ki.pressKey(hex_key.get('D'), 0.3)
 
 def move_character_each_15_minutes():
     while True:
+        print("Getting active roblox window")
         active_roblox_window()
+        print("Moving character")
         move_around_yourself()
+
+        print("Waiting 15 minutes for next move...")
         time.sleep(FIFTEEN_MINUTES)
 
-if __name__ == '__main__':
-    move_character_each_15_minutes()
+move_character_each_15_minutes()
